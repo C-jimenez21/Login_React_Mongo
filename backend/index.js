eb37158b5bbe58b5f0832cfa425e935a2f080aa8
C-jimenez21 <cristianjjc21@gmail.com>
2023-09-14 17:27:26 -0500

    feat: :sparkles: Create a schema of users

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import app from './app.js';
 import dotenv from 'dotenv';
 
 dotenv.config("")
-console.log(process.env.MY_SERVER);
 let server = JSON.parse(process.env.MY_SERVER)
 app.listen(server, () => {
     console.log(`Server listening on https://${server.hostname}:${server.port}`);
