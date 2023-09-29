8e7d6c95ed64ec084bd41a4e5801accba913d9e4
C-jimenez21 <cristianjjc21@gmail.com>
2023-09-29 17:14:52 -0500

    feat: :sparkles: create .env.example file

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,22 @@ import cors from 'cors';
 //import appPrueba from './routes/prueba.routes.js';
 import appAuth from './routes/auth.routes.js';
 const app = express(); 
+import dotenv from 'dotenv';
 
+dotenv.config('../')
+
+const URI = `http://${process.env.VITE_HOSTNAME}:${process.env.VITE_PORT_FRONTEND}`
 app.use(morgan('dev'));
 app.use(cors(
     {
-        origin: 'http://127.10.10.10:5010',
+        origin: URI,
         credentials:true
     }
 ));
 app.use(cookieParser());
 app.use(express.json())
 app.use('/', appAuth)
-
+    
 export default app
 
 
